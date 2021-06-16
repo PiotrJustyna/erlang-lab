b807ae69133318f5b34abe542e0aea9dfa4e93ad
Piotr Justyna <piotr.justyna@gmail.com>
2021-06-16 21:10:56 +0000

    Added if statement to the guards example.

--- 4_guards/guards/src/guards_app.erl
+++ 4_guards/guards/src/guards_app.erl
@@ -11,20 +11,33 @@
 
 start(_StartType, _StartArgs) ->
     io:format("~n"),
+
     io:format("both_variables_positive, positive test: ~s~n",
               [validator:both_variables_positive(1, 2)]),
     io:format("both_variables_positive, negative test: ~s~n",
               [validator:both_variables_positive(-1, 2)]),
+
+    io:format("~n"),
+
     io:format("any_variable_positive, positive test: ~s~n",
               [validator:any_variable_positive(1, 2)]),
     io:format("any_variable_positive, positive test: ~s~n",
               [validator:any_variable_positive(-1, 2)]),
     io:format("any_variable_positive, negative test: ~s~n",
               [validator:any_variable_positive(-1, -2)]),
-    io:format("are_equal, positive test: ~s~n",
-              [validator:are_equal(1, 1)]),
-    io:format("are_equal, negative test: ~s~n",
-              [validator:are_equal(1, 2)]),
+
+    io:format("~n"),
+
+    io:format("are_equal, positive test: ~s~n", [validator:are_equal(1, 1)]),
+    io:format("are_equal, negative test: ~s~n", [validator:are_equal(1, 2)]),
+
+    io:format("~n"),
+
+    io:format("compare, equal test: ~s~n", [validator:compare(1, 1)]),
+    io:format("compare, X > Y test: ~s~n", [validator:compare(2, 1)]),
+    io:format("compare, X < Y test: ~s~n", [validator:compare(1, 2)]),
+    io:format("compare, values cannot be compared test: ~s~n", [validator:compare(chair, 3)]),
+
     guards_sup:start_link().
 
 stop(_State) ->
--- 4_guards/guards/src/validator.erl
+++ 4_guards/guards/src/validator.erl
@@ -1,6 +1,6 @@
 -module(validator).
 
--export([both_variables_positive/2, any_variable_positive/2]).
+-export([both_variables_positive/2, any_variable_positive/2, are_equal/2, compare/2]).
 
 %% The key here is the comma in the guard - it acts as 'andalso'.
 both_variables_positive(X, Y) when X >= 0, Y >= 0 ->
@@ -15,6 +15,19 @@ any_variable_positive(_, _) ->
     false.
 
 are_equal(X, Y) ->
-    if X=:=Y -> true;
-    true -> false
-end.
+    if X =:= Y ->
+           true;
+       true ->
+           false
+    end.
+
+compare(X, Y) ->
+    if X =:= Y ->
+           both_values_equal;
+       X > Y ->
+           first_value_greater;
+       X < Y ->
+           second_value_greater;
+       true ->
+           values_cannot_be_compared
+    end.
